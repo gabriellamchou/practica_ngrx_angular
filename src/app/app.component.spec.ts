4b59185ad20bc6b90c9c35637d20fcdc4a835419
Gabriel <gabriel.lamela.ch@gmail.com>
2024-05-09 09:22:42 +0200

    Practicar ngrx

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title '13ngrx'`, () => {
+  it(`should have as title '6observables'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('13ngrx');
+    expect(app.title).toEqual('6observables');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('13ngrx app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('6observables app is running!');
   });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { init } from './counter-store/counter.actions';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = '13ngrx';
+export class AppComponent implements OnInit {
+  constructor(private store: Store){}
+
+  ngOnInit(): void {
+    this.store.dispatch(init());
+  }
 }
